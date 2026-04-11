a8398c595ffd665c9ed8224645d8b070c5bed260
ZeroIndent <josearaneda20244@gmail.com>
2026-04-11 08:08:13 +0000

    Mejora paleta y suaviza animaciones del fondo anime

--- artifacts/anime-web/src/App.tsx
+++ artifacts/anime-web/src/App.tsx
@@ -49,8 +49,6 @@ function AnimatedBackground() {
       <div className="orb orb-1" />
       <div className="orb orb-2" />
       <div className="orb orb-3" />
-      <div className="orb orb-4" />
-      <div className="orb orb-5" />
     </div>
   );
 }
